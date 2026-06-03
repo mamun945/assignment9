39ad053ae054091ab11b8b373a08f7b550150192
mamun945 <almamunab2001@gmail.com>
2026-06-03 22:39:37 +0600

    i have one problem solve

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,7 @@
 import MyRequest from "@/components/MyRequest";
 
 const MyRequestPage = async() => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/adoptioninfo`)
+  const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/adoptioninfo`,{ cache: 'no-store' })
   const petsAdoptionData = await res.json()
   console.log(petsAdoptionData);
   return (
