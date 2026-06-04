204b5c8ea4f2c6136b371526d5b037a1f01c0e0b
mamun945 <almamunab2001@gmail.com>
2026-06-04 09:00:17 +0600

    data lost have fixed

--- src/app/allpets/page.jsx
+++ src/app/allpets/page.jsx
@@ -4,7 +4,9 @@ import PetsCard from '@/components/PetsCard';
 import React from 'react'
 
 const AllPetsPage = async() => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/petsinfo`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/petsinfo`,{
+    cache: "no-store"
+  })
     const petsDatas = await res.json()
     console.log(petsDatas);
   return (
